6bb89efc5a2417c75c44c76d0e56ce1f5cbab1ec
mohan dandigam <mohandand@gmail.com>
2023-08-30 14:10:22 -0500

    Intial Layout and Routing is Completed

--- ecommerce-portal/src/App.js
+++ ecommerce-portal/src/App.js
@@ -1,11 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
-
+import './index.scss'
 import Home from './routes/home/home.component';
-
+import Navigation from './routes/navigation/navigation.component'
+import SignIn from './routes/sigin-in/sigin-in.component';
 const App = () => {
+
+  const Shop = () => {
+    return <div>I am Shop</div>
+  }
   return (
     <Routes>
-      <Route path='/home' index element={<Home />} />
+      <Route path='/' element={<Navigation/>}>
+      <Route  index element={<Home />} />
+      <Route  path="/shop" element={<Shop />} />
+      <Route  path="/sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 };
---
+++ ecommerce-portal/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import {ReactComponent as CrownLogo} from "../../assets/crown.svg"
+import './navigation.styles.scss'
+export default function Navigation() {
+    return (
+        <React.Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'><CrownLogo className="logo"/></Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to= '/shop'>SHOP</Link>
+                    <Link className='nav-link' to= '/sign-in'>SIGN IN</Link>
+                </div>
+            </div>
+            <Outlet />
+        </React.Fragment>
+    )
+}
+
