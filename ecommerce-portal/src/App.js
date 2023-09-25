2813b3bf0c3947b90532e481f5633b162e91f244
mohan dandigam <mohandand@gmail.com>
2023-09-24 22:12:09 -0500

    Completed Checkout part

--- ecommerce-portal/src/App.js
+++ ecommerce-portal/src/App.js
@@ -4,6 +4,7 @@ import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component'
 import Authenication from './routes/authenitcation/authenication.component';
 import Shop from './routes/shop/shop.component'
+import Checkout from './routes/checkout/checkout.component'
 const App = () => {
   return (
     <Routes>
@@ -11,6 +12,7 @@ const App = () => {
       <Route  index element={<Home />} />
       <Route  path="/shop" element={<Shop />} />
       <Route  path="/auth" element={<Authenication />} />
+      <Route  path="/checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
